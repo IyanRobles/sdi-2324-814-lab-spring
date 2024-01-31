db0b8314b40bd56db17adbcba3e12388a4020e78
Iyan Robles <UO288780@uniovi.es>
2024-01-31 23:53:54 +0100

    SDI-2324-814-2.1-Create Spring project.

---
+++ src/main/java/com/uniovi/sdi2324814spring/Sdi2324814SpringApplication.java
@@ -0,0 +1,13 @@
+package com.uniovi.sdi2324814spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sdi2324814SpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sdi2324814SpringApplication.class, args);
+	}
+
+}
