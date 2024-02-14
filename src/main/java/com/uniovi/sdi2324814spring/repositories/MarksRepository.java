85586dd06b295cdaa7921bd3569a866ccabc4338
Iyan Robles <UO288780@uniovi.es>
2024-02-14 23:45:01 +0100

    SDI-2324-814-4.4-SpringBoot-Consultas.

--- src/main/java/com/uniovi/sdi2324814spring/repositories/MarksRepository.java
+++ src/main/java/com/uniovi/sdi2324814spring/repositories/MarksRepository.java
@@ -1,14 +1,19 @@
 package com.uniovi.sdi2324814spring.repositories;
 
 import com.uniovi.sdi2324814spring.entities.Mark;
+import com.uniovi.sdi2324814spring.entities.User;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Repository
 public interface MarksRepository extends CrudRepository<Mark, Long> {
+    @Query("SELECT r FROM Mark r WHERE r.user = ?1 ORDER BY r.id ASC")
+    List<Mark> findAllByUser(User user);
     @Modifying
     @Transactional
     @Query("UPDATE Mark SET resend = ?1 WHERE id = ?2")
